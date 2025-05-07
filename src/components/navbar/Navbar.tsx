b55620dc9bafcaeb3c9d507c132e9593d289110d
Chinwenm Okorie <okoriechinwe1327@gmail.com>
2025-05-07 17:56:15 +0100

    w

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,8 +1,32 @@
-const Navbar: React.FC = ({})=>{
-    return(
-        <div>
-         nav
-        </div>
-    )
-}
+import Image from "next/image";
+import Link from "next/link";
+
+
+const Navbar: React.FC = ({}) => {
+  return (
+    <nav>
+      <div>
+        <Image src="" alt=""></Image>
+      </div>
+      <ul>
+        <li>
+            <Link href="#"></Link>
+        </li>
+        <li>
+            <Link href="#"></Link>
+        </li>
+        <li>
+            <Link href="#"></Link>
+        </li>
+        <li>
+            <Link href="#"></Link>
+        </li>
+        <li>
+            <Link href="#"></Link>
+        </li>
+      </ul>
+
+    </nav>
+  );
+};
 export default Navbar;
